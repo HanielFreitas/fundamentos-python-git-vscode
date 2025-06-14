afb610dcd3b1be5c8a03f985ad48b7f20d4e657d
HanielFreitas <hanielfreitas3@gmail.com>
2025-06-13 21:17:27 -0400

    feat: adiciona exemplos básicos de print, operações e input

---
+++ Exemplo01.py
@@ -0,0 +1,15 @@
+print("Hello, World!") #Print de um texto
+print("Olá" + " Mundo!") #Concatenação de strings
+print(3 + 4) #Soma de dois números
+print(3 * 4) #Multiplicação de dois números
+print(3 - 4) #Subtração de dois números
+print(3 / 4) #Divisão de dois números
+print(3 ** 4) #Exponenciação de dois números
+print(3 // 4) #Divisão inteira de dois números
+print(3 % 4) #Resto da divisão de dois números
+print("3" + "4") #Concatenação de strings numéricas
+print("3" + str(4)) #Concatenação de string com número convertido
+print("3" + str(4) + "5") #Concatenação de strings numéricas com conversão
+nome = input("Digite seu nome: ") #Entrada de dados do usuário com concatenação
+print ("Seu nome é " + nome) #Exibição do nome do usuário
+print("Seu nome tem o total de letras igual: "+ str(len(nome))) #Exibição do tamanho do nome do usuário
